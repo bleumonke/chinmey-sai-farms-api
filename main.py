7563664e2d52a4b82e1c06d812cf110bf27a0057
subramanyam mogili <czar1411@gmail.com>
2025-04-22 22:35:35 -0400

    Update CORS middleware to restrict origins and add Docker push script

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for route in __routes__:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can replace "*" with your frontend domain in production
+    allow_origins=["https://main.d2ut8kjo40hhn.amplifyapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,3 +23,8 @@ app.add_middleware(
 @app.get("/", tags=["Root"])
 def read_root():
     return {"message": "Chimney Farms API is running 🚜"}
+
+
+@app.get("/health", tags=["health"])
+def health():
+    return {"status": "healthy"}
---
+++ push.sh
@@ -0,0 +1,4 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 078548485543.dkr.ecr.us-east-1.amazonaws.com
+docker build -t chinmey-sai-farms-apis:1.0.1 .
+docker tag chinmey-sai-farms-apis:1.0.1 078548485543.dkr.ecr.us-east-1.amazonaws.com/chinmey-sai-farms-apis:1.0.1
+docker push 078548485543.dkr.ecr.us-east-1.amazonaws.com/chinmey-sai-farms-apis:1.0.1
