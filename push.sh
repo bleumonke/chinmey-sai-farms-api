fe975c0ca9fbee10ea972297f6de79bb82f274ee
subramanyam mogili <czar1411@gmail.com>
2025-06-22 13:59:17 -0400

    Update Docker image version to 1.0.0 in push script

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 078548485543.dkr.ecr.us-east-1.amazonaws.com
-docker build -t chinmey-sai-farms-apis:1.0.4 .
-docker tag chinmey-sai-farms-apis:1.0.4 078548485543.dkr.ecr.us-east-1.amazonaws.com/chinmey-sai-farms-apis:1.0.4
-docker push 078548485543.dkr.ecr.us-east-1.amazonaws.com/chinmey-sai-farms-apis:1.0.4
+docker build -t chinmey-sai-farms-apis:1.0.0 .
+docker tag chinmey-sai-farms-apis:1.0.0 078548485543.dkr.ecr.us-east-1.amazonaws.com/chinmey-sai-farms-apis:1.0.0
+docker push 078548485543.dkr.ecr.us-east-1.amazonaws.com/chinmey-sai-farms-apis:1.0.0
